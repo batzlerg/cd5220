263b7fc0b4f1d92d812e8f030cf80f49c634d92d
graham <batzler@pm.me>
2025-11-07 15:34:19 -0800

    add setup.py for installation as a module via pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="cd5220",
+    version="1.0.0",
+    py_modules=["cd5220", "animations"],
+    install_requires=["pyserial"],
+    python_requires=">=3.7",
+)
